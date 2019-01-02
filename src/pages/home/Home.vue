ec0db4f3b8804e1983f287aed7782c2a2d92e3b3
Holly <hollyhxy@126.com>
2019-01-02 16:50:54 +0800

    add swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-head></home-head>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import HomeHead from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHead
+    HomeHead,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: require('@/assets/images/1.jpg')
+      }, {
+        id: '002',
+        imgUrl: require('@/assets/images/2.jpg')
+      }, {
+        id: '003',
+        imgUrl: require('@/assets/images/3.jpg')
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 30.47%
+  background: #eef
+  .swiper-img
+    width: 100%
+</style>
